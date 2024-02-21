b8730a4301171f996ef3c170225a2fb10ce67446
simplywilber <simplywilber@gmail.com>
2024-02-21 11:30:39 +1000

    Create module 1 task text files for grading

--- classwork/module_3/asgmt_3/task1.py
+++ classwork/module_3/asgmt_3/task1.py
@@ -1,19 +1,27 @@
 # Write a python program to print sum of odd and even numbers up to n
 
-# Create a list
+# Created a list
 nlist = [1, 2, 3, 4, 5, 6, 7 ,8, 9 , 10]
 
-# Create empty variables
+# Created empty variables
 even_numbers_sum = 0
 odd_numbers_sum = 0
+
+# Created empty even and odd number lists
+even_numbers = []
+odd_numbers = []
 
 # Using modulo to find even numbers, else is odd
 for n in nlist:
     if n % 2 == 0:
+        even_numbers.append(n)
         even_numbers_sum += n
     else:
+        odd_numbers.append(n)
         odd_numbers_sum += n
 
 # Final output
+print("All even numbers are:", *even_numbers)
+print("All odd numbers are:", *odd_numbers)
 print(f"The sum of even numbers is: {even_numbers_sum}")
 print(f"The sum of odd numbers is: {odd_numbers_sum}")
