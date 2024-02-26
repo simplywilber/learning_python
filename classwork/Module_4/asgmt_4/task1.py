c9344d6f1311a12b4c453c409fb4f6f7754fd44d
simplywilber <simplywilber@gmail.com>
2024-02-26 12:17:05 +1000

    Begin task 1 for module 4 project

---
+++ classwork/Module_4/asgmt_4/task1.py
@@ -0,0 +1,6 @@
+nums = input("Enter elements of a list separated by space: ")
+
+str_extraction = [i for i in nums.split() if i.isdigit()]
+int_extraction = [int(i) for i in nums.split() if i.isdigit()]
+print(extraction)
+print(extraction)
