e8ed009a78b7761745ed8f7c02aacf18c398b8bb
simplywilber <simplywilber@gmail.com>
2024-03-05 16:27:09 +1000

    Create Midterm Exam Practice Folder

---
+++ classwork/MIDTERM_PRACTICAL_EXAM/practice.py
@@ -0,0 +1,51 @@
+# PRACTICE ALL LIST METHODS FOR THE EXAM
+# EXAM WILL BE HELD ON MARCH 12TH
+
+# LIST METHODS
+
+# append()
+# Adds an element at the end of the list
+
+# clear()
+# Removes all the elements from the list
+
+# copy()
+# Returns a copy of the list
+
+# count()
+# Returns the number of elements with the specified value
+
+# extend()
+# Add the elements of a list to the end of the current list
+
+# index()
+# Returns the index of the first element with the specified value
+
+# insert()
+# Adds an element at the specified position
+
+# remove()
+# Removes the first item with the specified value
+
+# reverse()
+# Reverse the order of the list
+
+# sort()
+# sorts the list
+
+# len()
+# Determines how many items a list has
+
+# type()
+# Shows the data type of the list
+
+
+
+
+
+
+n_list = [1, 2, 3, 4, 5, 6]
+f_list = ['Apples', 'Oranges', 'kiwis']
+copy_list = f_list.copy()
+print(copy_list)
+
