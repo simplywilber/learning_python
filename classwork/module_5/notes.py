c466f2cb789f38a328afcc63863672f3e5e6a1fb
simplywilber <simplywilber@gmail.com>
2024-03-19 16:20:52 +1000

    Create module 5 files

---
+++ classwork/module_5/notes.py
@@ -0,0 +1,13 @@
+# Tuple a collection which is ordered and unchangeable. Allows duplicate members!
+# Tuples use paranthesis
+# Tuples are faster than lists
+# Tuples can have on value which requires a comma at the end
+# Tuples can contain strings, integers and even lists within them
+mixed_objects = ("Hello", [1, 2], 3)
+one_number = (1,)
+numbers = (1, 2, 3)
+print(mixed_objects)
+print(numbers)
+print(one_number)
+
+# Set unchangeable and cannot duplicate
