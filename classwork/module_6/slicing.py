dee972d759604a9098190e1b320be82bf5d2f313
simplywilber <simplywilber@gmail.com>
2024-04-05 11:54:17 +1000

    Create module 6 practice file

---
+++ classwork/module_6/slicing.py
@@ -0,0 +1,13 @@
+# This is a practice using the slicing and find methods
+
+site_string = "http://en.wikipedia.org/wiki/Nasa/"
+
+print(f"The answer for a is: {site_string[10:19]}")
+print(f"The answer for b is: {site_string[10:-5]}")
+print(f"The answer for c is: {site_string[:23]}")
+print(f"The answer for d is: {site_string[:-1]}")
+
+# second activity
+
+my_string = "Hello World!"
+print(f"There are {my_string.count('!')} !'s")
