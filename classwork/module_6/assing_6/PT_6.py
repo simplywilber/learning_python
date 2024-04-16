6f2f489993a7ec2193834a0b5b0f3e1279c4be25
simplywilber <simplywilber@gmail.com>
2024-04-17 07:07:08 +1000

    Update assignment task 6 file

--- classwork/module_6/assing_6/PT_6.py
+++ classwork/module_6/assing_6/PT_6.py
@@ -1,3 +1,5 @@
+import time
+
 # Creating the menu
 menu_prompt=(
             """
@@ -15,12 +17,55 @@ menu_prompt=(
 while True: # exit when user enters no input
     command = input(menu_prompt).lower().strip()
     if command == '1':
-        print("1")
+        # Initial user input
+        user_input = list(map(int, input("Please enter a list of integers separated by spaces: ").split()))
 
+        # Creating a function for finding the smallest and second smallest integers
+        def finding_small_numbers(numbers):
+            
+            # Creating two values with positive infinity to be changed in the for loop
+            smallest = float('inf')
+            sec_smallest = float('inf')
+            
+            # Checking to see if there are at least two integers to find two small numbers
+            if len(user_input) < 2:
+                return "Please enter at least two integers"
+            else:
+                for num in numbers:
+                    if num < smallest:
+                        sec_smallest = smallest
+                        smallest = num
+                    elif num < sec_smallest:
+                        sec_smallest = num
+                
+                # Returning smallest and sec_smallest to display in the output
+                return smallest, sec_smallest
+            
+        # Finding the smallest and second smallest by providng the finding_small_numbers function an arguement
+        smallest, sec_smallest = finding_small_numbers(user_input)
+
+        # Printing the final output
+        print(f"The smallest integer is: {smallest}\nThe second smallest integer is: {sec_smallest}")
+        
+        # Using time delay for easy readability
+        time.sleep(1.2)
 
     elif command == '2':
-        print('2')
+            # Initial user input
+            user_input = input("Please enter your list of integers separated by spaces: ")
+            integers_list = [int(i) for i in user_input.split() if i.strip().isdigit()]
 
+            # Check if the number of inputs exceeds 9
+            if len(integers_list) > 9:
+                print("Too many inputs, please try again")
+            else:
+                # Find the middle item
+                middle_index = len(integers_list) // 2
+                middle_item = integers_list[middle_index]
+                print(f"The middle item is: {middle_item}")
+                
+                # Using time delay for easy readability
+                time.sleep(1.2)
 
     elif command == '3':
         # Creating an initial user input
@@ -45,8 +90,9 @@ while True: # exit when user enters no input
         # Calling the remove_odds() function
         remove_odds(initial_list)
         
+        # Using time delay for easy readability
+        time.sleep(1.2)
         
-
     elif command == '4':
         print(
             """
