7cbe660bea6666cbc31fd84d109fb4c0f0819396
simplywilber <simplywilber@gmail.com>
2024-04-23 16:59:40 +1000

    Update final project file

--- group_project/PythonProjectGroup4.py
+++ group_project/PythonProjectGroup4.py
@@ -1,83 +1,64 @@
-# initializing variables for user login
+
+# declaring variables
 username = ["Jerico", "Wilber"]
 user_pw = ["Estella", "Amaya"]
 x = 0
-attempts = 5
+attempts = 3
+def Capitalization_Correction():# function to quickly call it multiple times
+    sentence = input("Enter a sentence up to 50 characters: ")
+    if len(sentence) > 50:
+        print("Your sentence has more than 50 characters")
+        return  # returns to selection
+    fixed_sentence = sentence.capitalize()  # Capitalizes first word in the sentence and lowers the rest
+    if fixed_sentence[-1] not in ['.', '!', '?']:  # checks for end punctuation if not add period as default
+        fixed_sentence += '.'  # adds period
+    for i in range(len(fixed_sentence)-1):  # capitalizes after a punctuation
+        if fixed_sentence[i] in ['.', '!', '?']:
+            fixed_sentence = fixed_sentence[:i + 2] + fixed_sentence[i + 2:].capitalize()
+            # uncomment for the process
+            # print("Punctuation: ", fixed_sentence[i])
+            # print("after punctuation: ", fixed_sentence[:i + 2], fixed_sentence[:i + 2], "\n", fixed_sentence[i + 2:])
+    print("\n\nCorrection: " + fixed_sentence)
 
-
-# function to quickly call the capitalization correction function multiple times
-def Capitalization_Correction():
-    sentence = input("\nEnter a sentence up to 50 characters: ")[:50]
-    # Capitalizes first word in the sentence
-    fixed_sentence = sentence.capitalize()
-    # checks if there is no punctuation at end of sentence in list
-    # if not then a period is set as default
-    if fixed_sentence[-1] not in ['.', '!', '?']:
-        fixed_sentence += '.'
-        # checks for punctuation at end of sentence in list
-        # then starts the sentence correction
-    if fixed_sentence[-1] in ['.', '!', '?']:
-        # this for loop adds a capital
-        # for after in list and index is less than the length of fixed_sentence
-        for i in range(len(fixed_sentence)):
-            # if fixed sentence in list is true and index is less than
-            # the length of the sentence -1 then use Capitalize
-            if fixed_sentence[i] in ['.', '!', '?'] and i < len(fixed_sentence) - 1:
-                # uses capitalize function from 2 at 2 after a punctuation
-                fixed_sentence = fixed_sentence[:i + 2] + fixed_sentence[i + 2:].capitalize()
-    print(f"\nCorrected sentence: {fixed_sentence}")
-
-
-# while statements for attempts
-while attempts > 0:
-    attempt = f"""\n    You have {attempts} attempts left."""
-    print(attempt)
+while attempts > 0:  # while attempts greater than 5, continue
+    attempt = f"""\n    You have {attempts-1} attempts left."""  # formatting for when user failed login
     username_input = input("""
-     Enter username: """
-    )
+        Enter username: """
+                           )  # user inputs Username
     userpw_input = input("""
-     Enter Password: """
-    )
-    # if statements for username and password
+        Enter Password: """
+                         )  # user inputs Password
     if (username_input == username[0] and userpw_input == user_pw[0]) or (
-            username_input == username[1] and userpw_input == user_pw[1]):
-        attempts = 5
-        print(f"""\n
-==============================\n
-        Welcome, {username_input}\n
-==============================
-        """)
-        while x != '4':
+            username_input == username[1] and userpw_input == user_pw[1]):  # authentication login.
+        attempts = 0  # fix for exiting program
+        print(f"""
+            ==============================
+                    Welcome, {username_input}
+            ==============================""")
+        # while x not equal 4, continue
+        while x != '4':  # selection input
             print("""
-==============================\n
-          Main Menu\n
-  1) Enter 2 to begin problem #9\n
-  2) Enter 4 to log out\n
-==============================\n""")
+            ==============================
+                      Main Menu\n
+              1) Enter 2 to begin problem #9\n
+              2) Enter 4 to log out and exit\n
+            ==============================\n""")
             x = input("Please enter your selection: ")
             if x == '2':
-                Capitalization_Correction()
-            elif x == '4':
+                Capitalization_Correction()  # calls the function
+            elif x == '4':  # exits program
                 print("""
-==============================
-          Thank you!
-==============================""")
-                x = 0
+            ==============================
+                      Thank you!
+            ==============================""")
                 break
-        print("""
-    Please try again Later.""")
-    elif username_input == "" and userpw_input == "":
-        print("""
-Thank you for using sentence correction""")
-        break
+            else:
+                print("Please enter 2 or 4.")
     else:
-        attempts -= 1
+        attempts -= 1  # minus one after failed login
         if attempts > 0:
-            print("""
- Incorrect username or password.""")
+            print("\nIncorrect username or password.")
+            print(attempt)
         else:
-            print("""
-==============================\n
-   Please try again later.\n
-==============================
-  """)
+            print("\nPlease try again later.")
+
